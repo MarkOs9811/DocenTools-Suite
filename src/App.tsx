56ad83330c3b47af1604295eeac223b6bf407249
Marcos <marcosparitorres@gmail.com>
2026-06-09 12:34:45 -0500

    Solucion para ver

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,41 @@ import {
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./slices/store";
+
 import Dashboard from "./pages/Dashboard";
 import AppGeneradorGIFT from "./pages/AppGeneradorGIFT";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/app.css";
 import AppRecursosVisuales from "./pages/AppRecursosVisuales";
 import AppMentoria from "./pages/AppMentorIa";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/app.css";
+
 import { trackPageView } from "./lib/analytics";
 
-export default function App() {
+function AppRoutes() {
   const location = useLocation();
 
   useEffect(() => {
     trackPageView(location.pathname);
-  }, [location]);
+  }, [location.pathname]);
+
+  return (
+    <div className="container-fluid p-0 pb-5">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/gift" element={<AppGeneradorGIFT />} />
+        <Route path="/tags" element={<AppRecursosVisuales />} />
+        <Route path="/mentorIa" element={<AppMentoria />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default function App() {
   return (
     <Provider store={store}>
       <Router>
-        {/* El div contenedor se coloca por fuera de las Rutas */}
-        <div className="container-fluid p-0 pb-5">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/gift" element={<AppGeneradorGIFT />} />
-            <Route path="/tags" element={<AppRecursosVisuales />} />
-            <Route path="/mentorIa" element={<AppMentoria />} />
-          </Routes>
-        </div>
+        <AppRoutes />
       </Router>
     </Provider>
   );
